50a0a7314c26539982fb137b33c142de78280ae5
Won Seob Seo <pghoou@gmail.com>
2020-06-12 17:32:18 +0300

    enable debugging

--- src/renderer.js
+++ src/renderer.js
@@ -29,8 +29,6 @@
 import "./index.css";
 import * as React from "./Index.bs";
 
-console.log = function () {};
-
 var app = require("electron").remote;
 var dialog = app.dialog;
 var fs = require("fs");
